e7068ea510c93aba93ad7dd163d9629963887548
junning <jn8227805@gmail.com>
2025-05-11 16:13:21 +0200

    decouple the controller and the main function

--- iiwas_opsace.py
+++ iiwas_opsace.py
@@ -6,31 +6,7 @@ import pinocchio as pino
 from contact_estimator import high_gain_based_observer, kalman_disturbance_observer
 from mujoco_dyn import mujocoDyn
 from external_wrench import WrenchApplier
-
-# Cartesian impedance control gains.
-impedance_pos = np.asarray([100.0, 100.0, 100.0])  # [N/m]
-impedance_ori = np.asarray([50.0, 50.0, 50.0])  # [Nm/rad]
-
-# Joint impedance control gains.
-Kp_null = np.asarray([75.0, 75.0, 50.0, 50.0, 40.0, 25.0, 25.0])
-
-# Damping ratio for both Cartesian and joint impedance control.
-damping_ratio = 1.0
-
-# Gains for the twist computation. These should be between 0 and 1. 0 means no
-# movement, 1 means move the end-effector to the target in one integration step.
-Kpos: float = 0.95
-
-# Gain for the orientation component of the twist computation. This should be
-# between 0 and 1. 0 means no movement, 1 means move the end-effector to the target
-# orientation in one integration step.
-Kori: float = 0.95
-
-# Integration timestep in seconds.
-integration_dt: float = 1.0
-
-# Whether to enable gravity compensation.
-gravity_compensation: bool = True
+from controller import cartesian_impedance_nullspace
 
 # Simulation timestep in seconds.
 dt: float = 0.002
@@ -48,17 +24,6 @@ def main() -> None:
     pino_model = pino.buildModelFromMJCF("kuka_iiwa_14/iiwa14.xml")
     pino_data = pino_model.createData()
 
-    # Compute damping and stiffness matrices.
-    damping_pos = damping_ratio * 2 * np.sqrt(impedance_pos)
-    damping_ori = damping_ratio * 2 * np.sqrt(impedance_ori)
-    Kp = np.concatenate([impedance_pos, impedance_ori], axis=0)
-    Kd = np.concatenate([damping_pos, damping_ori], axis=0)
-    Kd_null = damping_ratio * 2 * np.sqrt(Kp_null)
-
-    # End-effector site we wish to control.
-    site_name = "attachment_site"
-    site_id = model.site(site_name).id
-
     # Get the dof and actuator ids for the joints we wish to control. These are copied
     # from the XML file. Feel free to comment out some joints to see the effect on
     # the controller.
@@ -71,26 +36,12 @@ def main() -> None:
         "joint6",
         "joint7",
     ]
-    dof_ids = np.array([model.joint(name).id for name in joint_names])
+    # dof_ids = np.array([model.joint(name).id for name in joint_names])
     actuator_ids = np.array([model.actuator(name).id for name in joint_names])
 
     # Initial joint configuration saved as a keyframe in the XML file.
     key_name = "home"
     key_id = model.key(key_name).id
-    q0 = model.key(key_name).qpos
-
-    # Mocap body we will control with our mouse.
-    mocap_name = "target"
-    mocap_id = model.body(mocap_name).mocapid[0]
-
-    # Pre-allocate numpy arrays.
-    jac = np.zeros((6, model.nv))
-    twist = np.zeros(6)
-    site_quat = np.zeros(4)
-    site_quat_conj = np.zeros(4)
-    error_quat = np.zeros(4)
-    M_inv = np.zeros((model.nv, model.nv))
-    Mx = np.zeros((6, 6))
 
     # Initialize the contact estimator
     gm_estimator = high_gain_based_observer(dt, "hg", model.nv)
@@ -127,26 +78,6 @@ def main() -> None:
         while viewer.is_running():
             step_start = time.time()
 
-            # Spatial velocity (aka twist).
-            dx = data.mocap_pos[mocap_id] - data.site(site_id).xpos
-            twist[:3] = Kpos * dx / integration_dt
-            mujoco.mju_mat2Quat(site_quat, data.site(site_id).xmat)
-            mujoco.mju_negQuat(site_quat_conj, site_quat)
-            mujoco.mju_mulQuat(error_quat, data.mocap_quat[mocap_id], site_quat_conj)
-            mujoco.mju_quat2Vel(twist[3:], error_quat, 1.0)
-            twist[3:] *= Kori / integration_dt
-
-            # Jacobian.
-            mujoco.mj_jacSite(model, data, jac[:3], jac[3:], site_id)
-
-            # Compute the task-space inertia matrix.
-            mujoco.mj_solveM(model, data, M_inv, np.eye(model.nv))
-            Mx_inv = jac @ M_inv @ jac.T
-            if abs(np.linalg.det(Mx_inv)) >= 1e-2:
-                Mx = np.linalg.inv(Mx_inv)
-            else:
-                Mx = np.linalg.pinv(Mx_inv, rcond=1e-2)
-
             # Apply external wrench to the end-effector.
             applied_external_wrench = wrench_applier.apply_wrench()
             
@@ -155,17 +86,8 @@ def main() -> None:
             # Compute all dynamic matrixes with MuJoCo
             g, M = mujoco_dyn.compute_all_forces()
             
-            # Compute generalized forces.
-            tau = jac.T @ Mx @ (Kp * twist - Kd * (jac @ data.qvel[dof_ids]))
-
-            # Add joint task in nullspace.
-            Jbar = M_inv @ jac.T @ Mx
-            ddq = Kp_null * (q0 - data.qpos[dof_ids]) - Kd_null * data.qvel[dof_ids]
-            tau += (np.eye(model.nv) - jac.T @ Jbar.T) @ ddq
-
-            # Add gravity compensation.
-            if gravity_compensation:
-                tau += data.qfrc_bias[dof_ids]
+            # Compute the control law.
+            tau = cartesian_impedance_nullspace(model, data, dt)
             
             # Update the generalized momentum observer
             gt_gm = M @ data.qvel
