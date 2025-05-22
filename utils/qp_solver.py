b124e7e4675ca005b324fe4614f90320fd4d2a32
junning <jn8227805@gmail.com>
2025-05-22 16:45:51 +0200

    add matrix based QP solver, not as fast as the sum of vectorized one

--- utils/qp_solver.py
+++ utils/qp_solver.py
@@ -84,7 +84,7 @@ class BatchQPSolver:
         self.f_c = cp.Variable((self.n_qps, 3 * self.n_contacts))  # [fx1, fy1, fz1, ...]
 
         # Define parameters for Jacobians and external torques
-        self.Jc = cp.Parameter((self.n_qps, 3, self.n_joints))  # Jacobian matrices for all QPs
+        self.Jc = cp.Parameter((self.n_qps, 3 * self.n_contacts, self.n_joints))  # Jacobian matrices for all QPs
         self.tau_ext = cp.Parameter((self.n_qps, self.n_joints))  # External torques for all QPs
 
         # Define the objective function (sum of squared residuals for all QPs)
@@ -131,6 +131,75 @@ class BatchQPSolver:
         # Return the results
         return self.f_c.value, self.objective.value
     
+class BatchQPSolverMatrix:
+    def __init__(self, n_joints, n_contacts, n_qps, mu=0.5):
+        """
+        Initialize the batch QP solver.
+
+        Args:
+            n_joints (int): Number of joints in the robot.
+            n_contacts (int): Number of contact points.
+            n_qps (int): Number of QPs to solve simultaneously.
+            mu (float): Friction coefficient.
+        """
+        self.n_joints = n_joints
+        self.n_contacts = n_contacts
+        self.n_qps = n_qps
+        self.mu = mu
+
+        # Define the contact force variable for all QPs
+        self.f_c = cp.Variable((self.n_qps * self.n_contacts * 3))  # [fx1, fy1, fz1, ...]
+
+        # Define parameters for Jacobians and external torques
+        self.Jc = cp.Parameter((self.n_qps * self.n_contacts * 3, self.n_qps * self.n_joints))  # Jacobian matrices for all QPs
+        self.tau_ext = cp.Parameter(self.n_qps * self.n_joints)  # External torques for all QPs
+        
+        # Define the objective function (sum of squared residuals for all QPs)
+        residuals = self.Jc.T @ self.f_c - self.tau_ext
+        self.objective = cp.Minimize(cp.sum_squares(residuals))
+        
+        # Define the constraints for all QPs
+        self.constraints = []
+        for i in range(self.n_qps):
+            start_idx = i * self.n_contacts * 3
+            for j in range(self.n_contacts):
+                fx = self.f_c[start_idx + 3 * j + 0]
+                fy = self.f_c[start_idx + 3 * j + 1]
+                fz = self.f_c[start_idx + 3 * j + 2]
+
+                # Unilateral constraint
+                self.constraints.append(fz >= 0)
+
+                # Friction cone (pyramid approximation)
+                self.constraints.append(cp.abs(fx) <= self.mu * fz)
+                self.constraints.append(cp.abs(fy) <= self.mu * fz)
+        
+        # Define the problem
+        self.prob = cp.Problem(self.objective, self.constraints)
+                
+    def solve(self, Jc_batch, tau_ext_batch):
+        """
+        Solve the batch QP problem with updated parameters.
+
+        Args:
+            Jc_batch (np.ndarray): Batch of Jacobian matrices of size (3, n_joints, n_qps).
+            tau_ext_batch (np.ndarray): Batch of external torques of size (n_joints, n_qps).
+
+        Returns:
+            np.ndarray: Estimated contact forces of size (3 * n_contacts, n_qps).
+            float: Loss value of the optimization problem.
+        """
+        # Update the parameters
+        self.Jc.value = Jc_batch
+        self.tau_ext.value = tau_ext_batch
+
+        # Solve the problem
+        self.prob.solve(solver=cp.OSQP, warm_start=True)
+
+        # Return the results
+        return self.f_c.value, self.objective.value
+            
+                
 if __name__ == "__main__":
     import numpy as np
     data_dict = np.load("data.npz")
@@ -147,6 +216,8 @@ if __name__ == "__main__":
     qp_solver = QPSolver(n_joints, n_contacts, mu)
     gt_ext_taus = []
     fcs = []
+    Jcs = []
+    objective_values = []
     for i in range(100, 200):
         index = i
         Jc = data_dict["jacs_body"][index]
@@ -155,15 +226,14 @@ if __name__ == "__main__":
         ext_wrench = data_dict["gt_ext_wrenches"][index]
         gt_ext_tau = Jc.T @ ext_wrench
         gt_ext_taus.append(gt_ext_tau)
-        # print(gt_ext_tau, ext_tau, ext_wrench)
+        Jcs.append(Jc)
         
         # Solve the QP problem
         Jc = Jc[:3, :]
 
         f_c, objective_value = qp_solver.solve(Jc, gt_ext_tau)
-        fcs.append(f_c)
-        # print("Contact forces:", f_c)
-        # print("Objective value:", objective_value)
+        objective_values.append(objective_value)
+    print(np.mean(objective_values))
     end_time = time.time()
     print("Time taken to solve QP:", end_time - start_time)
     
@@ -173,10 +243,28 @@ if __name__ == "__main__":
     n_qps = 100  # Number of QPs to solve simultaneously
     mu = 0.5
     Jc_batch = np.array(data_dict["jacs_body"][100:100+n_qps][:, :3, :])
-    gt_ext_tau = np.array(gt_ext_taus)
+    gt_ext_tau = np.array(gt_ext_taus[: 100])
     batch_qp_solver = BatchQPSolver(n_joints, n_contacts, n_qps, mu)
-    start_time = time.time()
-    f_c_batch, loss = batch_qp_solver.solve(Jc_batch, gt_ext_tau)
-    end_time = time.time()
-    print("Batch QP solver time:", end_time - start_time)
-    print(loss)
+    
+    for i in range(3):
+        start_time = time.time()
+        f_c_batch, loss = batch_qp_solver.solve(Jc_batch, gt_ext_tau)
+        end_time = time.time()
+        print("Batch QP solver time:", end_time - start_time)
+        print(loss)
+    
+    # Example data    
+    Jc_batch = np.array(data_dict["jacs_body"][100:100+n_qps][:, :3, :])
+    gt_ext_tau = np.array(gt_ext_taus[: 100])
+    from scipy.linalg import block_diag
+    Jc_batch = block_diag(*Jc_batch)
+    gt_ext_tau = gt_ext_tau.flatten()
+    batch_qp_solver2 = BatchQPSolverMatrix(n_joints, n_contacts, n_qps, mu)
+    print(Jc_batch.shape, gt_ext_tau.shape)
+    
+    for i in range(3):
+        start_time = time.time()
+        f_c_batch, loss = batch_qp_solver2.solve(Jc_batch, gt_ext_tau)
+        end_time = time.time()
+        print("Batch QP solver2 time:", end_time - start_time)
+        print(loss)
