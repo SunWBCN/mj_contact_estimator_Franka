8292ca30902cd9398167bf063fa13c0bbcd8b0ed
junning <jn8227805@gmail.com>
2025-08-21 15:51:04 +0200

    bug fix for preprocessing mesh & generate distances for geometric distances

--- utils/preprocess_mesh.py
+++ utils/preprocess_mesh.py
@@ -44,30 +44,18 @@ def compute_face_centroids(vertices, faces):
 def test_if_geodesic_computable(points, faces, valid_vertex_indices):
     geoalg = geodesic.PyGeodesicAlgorithmExact(points, faces)
     invalid_vertex_indexes = []
+    distances = np.ones(shape=(len(points), len(points)), dtype=np.float64) * -1  # shape (n_points, n_points)
     for i in trange(len(points)):
         sourceIndex = np.array([i], dtype=np.int64)
         distances_, best_source_ = geoalg.geodesicDistances(sourceIndex)
         sliced_distances = distances_[valid_vertex_indices]
         if np.any(sliced_distances == np.inf):
             invalid_vertex_indexes.extend(sourceIndex)
+        distances[i, :] = distances_
     if len(invalid_vertex_indexes) > 0:
-        return False, invalid_vertex_indexes
+        return False, invalid_vertex_indexes, distances
     else:
-        return True, []
-
-def test_only_if_geodesic_computable(points, faces, valid_face_indices, valid_vertex_indices):
-    geoalg = geodesic.PyGeodesicAlgorithmExact(points, faces)
-    for sourceFaceIndex in trange(len(faces), desc="Testing geodesic computability"):
-        if sourceFaceIndex not in valid_face_indices:
-            continue
-        face = faces[sourceFaceIndex]
-        for vertexIndex in face:
-            sourceIndex = np.array([vertexIndex], dtype=np.int64)
-            distances_, best_source_ = geoalg.geodesicDistances(sourceIndex)
-            slices_distances = distances_[valid_vertex_indices]
-            if np.any(slices_distances == np.inf):
-                return False
-    return True
+        return True, [], distances
 
 if __name__ == "__main__":
     # Read the text.txt file and retreive vertex information
@@ -104,7 +92,7 @@ if __name__ == "__main__":
     # visualize_mesh_indexes(vertices=vertices, faces=faces, faces_indexes=valid_face_indices)
     valid_vertex_indices = np.setdiff1d(np.arange(len(vertices)), vertex_indices)    
 
-    is_comp, invalid_vertex_indexes = test_if_geodesic_computable(vertices, faces, valid_vertex_indices)
+    is_comp, invalid_vertex_indexes, distances = test_if_geodesic_computable(vertices, faces, valid_vertex_indices)
     print("Is geodesic computable:", is_comp, invalid_vertex_indexes, len(invalid_vertex_indexes))
 
     # remove also the not computable face indexes in valid_face_indices
@@ -114,14 +102,50 @@ if __name__ == "__main__":
     valid_face_indices = np.setdiff1d(valid_face_indices, invalid_face_indices)
     print(len(valid_face_indices), "After filtering, Face")
     
-    is_comp = test_only_if_geodesic_computable(vertices, faces, valid_face_indices, valid_vertex_indices)
-    if not is_comp:
-        raise ValueError(f"Geodesic distance computation is not valid for the mesh {mesh_name}, invalid face indexes: {invalid_face_indices}, invalid vertex indexes: {invalid_vertex_indexes}")
-    else:
-        print("Geodesic distance computation is valid")
+    # test if the vertexes computation
+    sliced_distances = distances[np.ix_(valid_vertex_indices, valid_vertex_indices)]
+    if np.any(sliced_distances == np.inf):
+        raise ValueError(f"Geodesic distance for vertices contains inf values, indicating disconnected components")
+
+    # generating geodesic distance table
+    distances_faces = np.ones(shape=(len(faces), len(faces)), dtype=np.float64) * -1  # shape (n_faces, n_faces)
+    face_dis_repre = "min"
+    for i in trange(len(valid_face_indices), desc=f"Computing geodesic distances for faces for {mesh_name}"):
+        sourceFaceIndex = valid_face_indices[i]
+        for targetFaceIndex in valid_face_indices:
+            face_source = faces[sourceFaceIndex]
+            face_target = faces[targetFaceIndex]
+            if sourceFaceIndex == targetFaceIndex:
+                distances_faces[sourceFaceIndex, targetFaceIndex] = 0.0
+            else:
+                distances_c = []
+                for v_source in face_source:
+                    for v_target in face_target:
+                        distances_c.append(distances[v_source, v_target])
+                if face_dis_repre == "min":
+                    distances_faces[sourceFaceIndex, targetFaceIndex] = np.min(distances_c)
+                elif face_dis_repre == "mean":
+                    distances_faces[sourceFaceIndex, targetFaceIndex] = np.mean(distances_c)
+                    
+    sliced_distances_faces = distances_faces[np.ix_(valid_face_indices, valid_face_indices)]
+    if np.any(sliced_distances_faces == np.inf):
+        raise ValueError(f"Geodesic distance for faces contains inf values, indicating disconnected components")
+    if np.any(sliced_distances_faces == -1):
+        raise ValueError(f"Geodesic distance for faces contains -1 values, indicating uncomputed distances")
+
+    np.savez_compressed(f"../kuka_iiwa_14/mesh_geodesic/distance_{mesh_name}.npz",
+                        distances=distances,
+                        sliced_distances=sliced_distances,
+                        distances_faces=distances_faces,
+                        distances_faces_sliced=sliced_distances_faces,
+                        valid_vertex_indices=valid_vertex_indices,
+                        valid_face_indices=valid_face_indices,
+                        )
 
     # visualize it
     visualize_mesh_indexes(vertices=vertices, faces=faces, faces_indexes=valid_face_indices)
+    
+    # preprocess boundary regions
     idx_path = f"{Path(__file__).resolve().parent}/../kuka_iiwa_14/mesh_data/{mesh_name}_mujoco_cached_nom_ind.npy"
     nom_vertex_indices = np.load(idx_path)
     for vertex_index in vertex_indices:
@@ -134,8 +158,13 @@ if __name__ == "__main__":
     # Remove face indices that are not in the valid face indices
     nom_face_indices = np.intersect1d(face_indices, valid_face_indices)
 
-    # show the faces indexes that need to be deleted
-    visualize_mesh_indexes(vertices=vertices, faces=faces, faces_indexes=nom_face_indices)
+    # check nom_face_indices can not be empty
+    if len(nom_face_indices) == 0:
+        print(f"No non-manifold face indices found for {mesh_name}.")
+        exit(0)
+
+    # # show the faces indexes that need to be deleted
+    # visualize_mesh_indexes(vertices=vertices, faces=faces, faces_indexes=nom_face_indices)
 
     if np.any(np.isin(invalid_face_indices, nom_face_indices)) or np.any(np.isin(invalid_face_indices, valid_face_indices)):
         raise ValueError("Invalid face indexes found in non-manifold face indices or valid face indices")
@@ -146,3 +175,4 @@ if __name__ == "__main__":
     np.save(f"{dir_path}/{mesh_name}_valid_face_indices.npy", valid_face_indices)
     np.save(f"{dir_path}/{mesh_name}_non_manifold_face_indices.npy", nom_face_indices)
     np.save(f"{dir_path}/{mesh_name}_non_manifold_vertex_indices.npy", nom_vertex_indices)
+    
